6796697384fe94692b2d106e6b3db88ab5d7e104
xiang.su <xiang.su@qq.com>
2018-11-03 01:48:15 +0800

    过滤分表,不显示重复数据结构

--- application/common/code_doc/DatabaseSchema.php
+++ application/common/code_doc/DatabaseSchema.php
@@ -16,10 +16,25 @@ class DatabaseSchema
         $rs = Db::query('SHOW TABLE STATUS');
         $rt = array_column($rs, null, 'Name');
         foreach ($rt as $table => $cells) {
+            if(self::isPartitionTable($table)){
+                unset($rt[$table]);
+                continue;
+            }
             $sql = "SHOW FULL COLUMNS FROM `$cells[Name]`";
             $cols = Db::query($sql);
             $rt[$table]['detail'] = array_column($cols, null, 'Field');
         }
         return $rt;
+    }
+
+    /**
+     * 过滤分表,不显示重复数据结构
+     * @param string $tableName
+     * @return bool
+     */
+    private static function isPartitionTable(string $tableName): bool
+    {
+        preg_match("/\_\d+$/", $tableName, $out);
+        return (bool)$out;
     }
 }
